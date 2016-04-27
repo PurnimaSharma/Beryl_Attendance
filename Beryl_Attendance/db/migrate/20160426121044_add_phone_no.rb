328340cfcd2a5ca7e0906da1bf3541220d9c71ca
PurnimaSharma <purnima.sharma@berylsystems.com>
2016-04-27 12:59:06 +0530

    removed Gemfile,Gemlock, byebug,migrate

--- Beryl_Attendance/db/migrate/20160426121044_add_phone_no.rb
+++
@@ -1,5 +0,0 @@
-class AddPhoneNo < ActiveRecord::Migration
-  def change
-  	add_column :employees, :phoneno, :string
-  end
-end
--- Beryl_Attendance/db/migrate/20160427051525_update.rb
+++
@@ -1,5 +0,0 @@
-class Update < ActiveRecord::Migration
-  def change
-  	change_column :employees, :sign_in_count, :integer, :default => 0
-  end
-end
